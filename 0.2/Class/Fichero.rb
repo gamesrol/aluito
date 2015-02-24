7e59b28f07dfa6dfb8c1e76acd599c2fdfa10a4c
Gamesrol <gs.gore@gmail.com>
2015-02-24 01:29:14 +0100

    mas cosas

--- 0.2/Class/Fichero.rb
+++ 0.2/Class/Fichero.rb
@@ -6,20 +6,20 @@ class Fichero
 	
 	def write(a, b)
 		text = ""
-		File.open('resultados.dat', 'r') do |f1|
+		File.open('registro.dat', 'r') do |f1|
 			while linea = f1.gets
 				text = text + linea
 			end
 		end
 
-		File.open('resultados.dat', 'w') do |f|
+		File.open('registro.dat', 'w') do |f|
 			f.puts text
 			f.puts a+";"+b
 		end
 	end
 
 	def read()
-		File.open('.dat', 'r') do |f1|
+		File.open('registro.dat', 'r') do |f1|
 			while linea = f1.gets
 				resultado = linea.split(";") 
 			end
